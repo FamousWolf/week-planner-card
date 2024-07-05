781019528a83644f1e543ce0eb7c22bb1b30823b
Alistair Marshall <git@a14m.uk>
2024-07-05 15:31:00 +0100

    Add day of week class to days
    Update _getDayClass to include the day of week. Could be useful to style weekends differently. #96

--- src/card.js
+++ src/card.js
@@ -556,6 +556,7 @@ export class WeekPlannerCard extends LitElement {
                 classes.push('past');
             }
         }
+        classes.push(["sunday","monday","tuesday","wednesday","thursday","friday","saturday"][startDate.getDay()])
         return classes.join(' ');
     }
 
